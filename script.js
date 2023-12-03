234822145b902007205c94ccaa7f709117d784e9
Sabareesh G <123008085+G-Sabareesh@users.noreply.github.com>
2023-12-03 17:58:15 +0530

    Revamp Visuals and Update Code
    Improved JavaScript functionality for real-time updates of time, date, and day.
    Updated HTML code to seamlessly integrate new day and date content.
    Updated styles to provide a more informative and visually appealing display.
    Addressed bugs in CSS and JavaScript for a smoother user experience.

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
-const now = new Date();
+var now = 0;
+var meridiem = document.getElementById('ampm'),
+    hour = document.getElementById('hour'),
+    min = document.getElementById('min'),
+    sec = document.getElementById('sec');
 function updateClock() {
 
+    now = new Date();
     // Get hours in 24-hour format
     let hours24 = now.getHours();
 
     // Convert to 12-hour format
     let hours12 = (hours24 % 12) || 12;
 
+    const hours = hours12.toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
 
     // Determine if it's AM or PM
-    const ampm = hours24 < 12 ? 'AM' : 'PM';
+    const ampm = hours24 < 12 ? 'am' : 'pm';
 
-    const timeString = `${hours12}:${minutes}:${seconds}`;
-
-    document.getElementById('displayTime').textContent = timeString;
+    hour.textContent = hours;
+    min.textContent = minutes;
+    sec.textContent = seconds;
+    meridiem.textContent = ampm;
 }
 
-// setInterval(updateClock, 1000);
-// updateClock(); // Initial call to display time immediately
+setInterval(updateClock, 1000);
 
-function todayDate() {
+function todayDateday() {
+    console.log('hi')
     const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
     const dayOfWeek = daysOfWeek[now.getDay()];
     document.querySelector(`.${dayOfWeek}`).classList.add('active');
+
+    const year = now.getFullYear().toString().padStart(2, '0');
+    const month = now.getMonth() + 1;
+    const day = now.getDate().toString().padStart(2, '0');
+    const date = `${day}-${month.toString().padStart(2, '0')}-${year}`
+    document.getElementById('date').textContent = date;
 }
+
+updateClock(); // Initial call to display time immediatelys
+todayDateday();
